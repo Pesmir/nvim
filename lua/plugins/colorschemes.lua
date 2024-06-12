2980b9aa28b89ce732fcf44a58e554a9edc5eb65
Pesmir <johannes.pesenhofer@student.tugraz.at>
2024-06-12 22:56:21 +0200

    updated june 2024

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,13 +1,9 @@
 return {
-  "Shatur/neovim-ayu",
-  lazy = false,
-  config = function()
-    local cmd = vim.cmd
-    local opt = vim.opt
-    opt.termguicolors = true -- enable 24-bit RGB colors
-    require("ayu").setup({
-      mirage = true,
-    })
-    require("ayu").colorscheme()
-  end,
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
 }
